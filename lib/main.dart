264925ea987b4bcfd39bfdb97733623cc3a44490
krishnadwypayan <krishnadwypayan12@gmail.com>
2019-03-30 22:46:26 +0530

    Home screen Renderflex issues resolved on small screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'home_screen.dart';
 
 void main() => runApp(MyDiaryApp());
 
@@ -27,8 +28,10 @@ class LoginScreen extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          SizedBox(height: 280.0,),
+          SizedBox(height: MediaQuery.of(context).size.height *.25,),
           Column(
             children: <Widget>[
               Container(
@@ -44,66 +47,72 @@ class LoginScreen extends StatelessWidget {
                     SizedBox(height: 10.0,),
                     Text(
                       'Get started by logging into\nyour account.',
-                      style: TextStyle(fontSize: 18.0, color: primaryColor),
+                      style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: primaryColor),
                     ),
                   ],
                 ),
               ),
             ],
           ),
-          SizedBox(height: 180.0,),
-          Container(
-            padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0, bottom: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                SocialSitesLoginButton('assets/images/fb.png', 'Facebook'),
-                SizedBox(width: 20.0,),
-                SocialSitesLoginButton('assets/images/google.png', 'Google'),
-              ],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          SizedBox(height: 80.0,),
+          Column(
             children: <Widget>[
-              CustomPaint(painter: DrawHorizontalLine(true)),
-              Text('or'),
-              CustomPaint(painter: DrawHorizontalLine(false)),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0, bottom: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: primaryColor,
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  ),
-                  height: 50.0,
-                  width: 350.0,
-                  child: Center(child: Text('Signup with Email', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),)),
-                )
-              ],
-            ),
-          ),
-          SizedBox(height: 10.0,),
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Existing user?', style: TextStyle(fontSize: 14.0, color: Colors.black),),
-                RaisedButton(
-                  onPressed: () {},
-                  color: backgroundColor,
-                  elevation: 0.0,
-                  child: Text('Login now', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: primaryColor),),
+              Container(
+                padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0, bottom: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SocialSitesLoginButton('assets/images/fb.png', 'Facebook'),
+                    SizedBox(width: 20.0,),
+                    SocialSitesLoginButton('assets/images/google.png', 'Google'),
+                  ],
                 ),
-              ],
-            )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomPaint(painter: DrawHorizontalLine(true)),
+                  Text('or'),
+                  CustomPaint(painter: DrawHorizontalLine(false)),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0, bottom: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: primaryColor,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      height: 50.0,
+                      width: 350.0,
+                      child: Center(child: Text('Signup with Email', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),)),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Existing user?', style: TextStyle(fontSize: 14.0, color: Colors.black),),
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                        },
+                        color: backgroundColor,
+                        elevation: 0.0,
+                        child: Text('Login now', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: primaryColor),),
+                      ),
+                    ],
+                  )
+              ),
+            ],
           ),
         ],
       ),
